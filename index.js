8a773b251a7e07cbb61407041b0835a9261d5d91
alwandexa <alwan.wirawan@djembatandua.com>
2024-01-18 17:25:45 +0700

    fix addItem function

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const addItem = (event) => {
-    const formData = new FormData(document.getElementById('item-form'))
-  
-    const table = document.getElementById("item-table").getElementsByTagName('tbody')[0];
-  
-    let newRow = table.insertRow(0);
-  
-    let nameCell = newRow.insertCell(0);
-    let amountCell = newRow.insertCell(1);
-  
-    nameCell.innerHTML = formData.get('item-name');
-    amountCell.innerHTML = formData.get('item-amount');
+  const form = document.getElementById("item-form");
+  const formData = new FormData(form);
 
-    document.getElementById("item-form").reset();
+  const table = document
+    .getElementById("item-table")
+    .getElementsByTagName("tbody")[0];
 
-    event.preventDefault();
-}
+  let newRow = table.insertRow(0);
 
-document.getElementById("item-form").onsubmit = addItem
+  let nameCell = newRow.insertCell(0);
+  let amountCell = newRow.insertCell(1);
+
+  nameCell.innerHTML = formData.get("item-name");
+  amountCell.innerHTML = formData.get("item-amount");
+
+  form.reset();
+
+  event.preventDefault();
+};
+
+document.getElementById("item-form").onsubmit = addItem;
