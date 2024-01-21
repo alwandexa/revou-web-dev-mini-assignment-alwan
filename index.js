15cfcba5a0e9dc1bdfb0b19c04061582db9f13eb
alwandexa <alwan.wirawan@djembatandua.com>
2024-01-22 06:08:37 +0700

    add delete function

--- index.js
+++ index.js
@@ -10,13 +10,35 @@ const addItem = (event) => {
 
   let nameCell = newRow.insertCell(0);
   let amountCell = newRow.insertCell(1);
+  let deleteCell = newRow.insertCell(2);
 
   nameCell.innerHTML = formData.get("item-name");
   amountCell.innerHTML = formData.get("item-amount");
+  deleteCell.innerHTML = `<button class="delete-button"><svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+</svg></button>`;
 
   form.reset();
+
+  handleDeleteButton();
 
   event.preventDefault();
 };
 
+const handleDeleteButton = () => {
+  let deleteButtons = document.querySelectorAll(".delete-button");
+
+  deleteButtons.forEach(function (button) {
+    button.addEventListener("click", deleteItem);
+  });
+};
+
+const deleteItem = (event) => {
+  const rowIndex = event.target.parentNode.parentNode.parentNode.rowIndex;
+  document.getElementById("item-table").deleteRow(rowIndex);
+};
+
 document.getElementById("item-form").onsubmit = addItem;
+
+handleDeleteButton();
